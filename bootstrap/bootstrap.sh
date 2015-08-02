c5577e0542cefc9618df16a466991acff4b1d947
Michael Allen <michael@michaelallen.io>
2015-08-02 21:18:21 +0100

    Quieten the gem install

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -25,5 +25,5 @@ git submodule update --init --recursive >/dev/null
 echo "Boostrapping puppet..."
 apt-get install -y puppet >/dev/null
 apt-get install -y rubygems >/dev/null
-gem install --no-ri --no-rdoc rubygems-update
+gem install --no-ri --no-rdoc rubygems-update >/dev/null
 update_rubygems >/dev/null
