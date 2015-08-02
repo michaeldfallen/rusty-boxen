5e027706987c690b144b0735b346972ee30274a3
Michael Allen <michael@michaelallen.io>
2015-08-02 17:12:39 +0100

    initial puppet bootstrap

---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Bootstraps Rusty from 0 to puppet
+
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root." >&2
+  exit 1
+fi
+
+echo "Installing git..."
+apt-get install -y git >/dev/null
+
+echo "Cloning rusty-boxen to /opt/rusty-boxen..."
+git clone --recurse-submodules https://github.com/michaeldfallen/rusty-boxen.git /opt/rusty-boxen
+
+# cd /opt/rusty-boxen
+
+# echo "Boostrapping puppet..."
+# ./bootstrap/puppet-bootstrap/ubuntu.sh
