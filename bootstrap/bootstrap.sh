587584c8fadb6871f5040974d8e034b740ab39e4
Michael Allen <michael@michaelallen.io>
2015-08-02 17:51:02 +0100

    Only clone if the repo doesn't exist

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -10,10 +10,15 @@ fi
 echo "Installing git..."
 apt-get install -y git >/dev/null
 
-echo "Cloning rusty-boxen to /opt/rusty-boxen..."
-git clone --recurse-submodules --force https://github.com/michaeldfallen/rusty-boxen.git /opt/rusty-boxen >/dev/null
+if [ ! -d /opt/rusty-boxen ]; then
+  echo "Cloning rusty-boxen to /opt/rusty-boxen..."
+  git clone --recurse-submodules https://github.com/michaeldfallen/rusty-boxen.git /opt/rusty-boxen >/dev/null
+fi
 
 cd /opt/rusty-boxen
+git fetch >/dev/null
+git pull >/dev/null
+git submodule update --init --recursive >/dev/null
 
 echo "Boostrapping puppet..."
 chmod u+x ./bootstrap/puppet-bootstrap/ubuntu.sh
