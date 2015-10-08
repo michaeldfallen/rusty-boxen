0631d511b2abea569b3948a251f67bf5c7c6f5c4
Michael Allen <michael@michaelallen.io>
2015-10-08 15:56:19 +0100

    reload upstart if file changes

--- modules/auto_puppet_apply/manifests/init.pp
+++ modules/auto_puppet_apply/manifests/init.pp
@@ -4,5 +4,11 @@ class auto_puppet_apply {
   file { '/etc/init/boxen.conf':
     ensure => 'link',
     target => '/opt/rusty-boxen/modules/auto_puppet_apply/files/upstart.conf',
+    notify => Exec['my_command'],
+  }
+
+  exec { 'initctl reload-configuration':
+    refreshonly => true,
+    require     => File['/etc/init/boxen.conf'],
   }
 }
