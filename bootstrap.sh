41960b61ecb2fcc30bcb30c7b7cc2491f3f9e338
Michael Allen <michael@michaelallen.io>
2015-10-09 10:49:34 +0100

    Need to be in the repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,14 +7,14 @@ sudo -v
 echo "Installing git..."
 sudo apt-get install -y git >/dev/null
 
-echo "Create ssh key..."
-./bin/newsshkey "github.com"
-read -p "Upload ssh key to Github then press any key to continue... " -n1 -s
-
 echo "Cloning rusty-boxen to /tmp/rusty-boxen..."
 git clone --recurse-submodules https://github.com/michaeldfallen/rusty-boxen.git /tmp/rusty-boxen >/dev/null
 
 cd /tmp/rusty-boxen
+
+echo "Create ssh key..."
+./bin/newsshkey "github.com"
+read -p "Upload ssh key to Github then press any key to continue... " -n1 -s
 
 echo "Boostrapping puppet..."
 sudo apt-get install -y puppet >/dev/null
