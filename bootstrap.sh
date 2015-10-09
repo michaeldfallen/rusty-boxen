eb7c6858c47cb3008f434f38bf4ad06db7780f50
Michael Allen <michael@michaelallen.io>
2015-10-09 10:47:57 +0100

    Automate the sshkey generation

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,10 @@ sudo -v
 echo "Installing git..."
 sudo apt-get install -y git >/dev/null
 
+echo "Create ssh key..."
+./bin/newsshkey "github.com"
+read -p "Upload ssh key to Github then press any key to continue... " -n1 -s
+
 echo "Cloning rusty-boxen to /tmp/rusty-boxen..."
 git clone --recurse-submodules https://github.com/michaeldfallen/rusty-boxen.git /tmp/rusty-boxen >/dev/null
 
