1b7b9c54d4a9ec114c819b0b615844a936d94253
Michael Allen <michael@michaelallen.io>
2015-10-09 10:10:08 +0100

    Missed change of module name in link

--- modules/rusty_boxen/manifests/init.pp
+++ modules/rusty_boxen/manifests/init.pp
@@ -9,7 +9,7 @@ class rusty_boxen {
 
   file { '/etc/init/boxen.conf':
     ensure => 'link',
-    target => '/opt/rusty-boxen/modules/auto_puppet_apply/files/upstart.conf',
+    target => '/opt/rusty-boxen/modules/rusty_boxen/files/upstart.conf',
     notify => Exec['initctl-reload'],
   }
 
