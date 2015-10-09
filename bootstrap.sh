65de8715f0b78f66f2b19de5dade14a3044ff160
Michael Allen <michael@michaelallen.io>
2015-10-09 13:08:41 +0100

    Replacing sudo in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,9 @@
 #
 # Bootstraps Rusty from 0 to puppet
 
-sudo -v
-
 echo "Installing git..."
-sudo apt-get install -y git >/dev/null
-sudo apt-get install -y xclip >/dev/null
+apt-get install -y git >/dev/null
+apt-get install -y xclip >/dev/null
 
 echo "Cloning rusty-boxen to /opt/rusty-boxen..."
 git clone --recurse-submodules https://github.com/michaeldfallen/rusty-boxen.git /opt/rusty-boxen
@@ -18,10 +16,10 @@ echo "Create ssh key..."
 read -p "Upload ssh key to Github then press any key to continue... " -n1 -s </dev/tty
 
 echo "Boostrapping puppet..."
-sudo apt-get install -y puppet >/dev/null
+apt-get install -y puppet >/dev/null
 
 echo "Running hacks..."
-sudo ./hacks/facter_fqdn.sh
+./hacks/facter_fqdn.sh
 
 echo "Running rusty..."
-sudo /opt/rusty-boxen/bin/rusty update
+/opt/rusty-boxen/bin/rusty update
