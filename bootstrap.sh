88f3c351dcddf74cf3f79fb71cdda3e38847406e
Michael Allen <michael@michaelallen.io>
2015-10-12 15:57:45 +0100

    Make bootstrap setup rusty so user can work with it

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,15 @@
 #
 # Bootstraps Rusty from 0 to puppet
 
+sudo -v
+
 echo "Installing git..."
-apt-get install -y git >/dev/null
-apt-get install -y xclip >/dev/null
+sudo apt-get install -y git >/dev/null
+sudo apt-get install -y xclip >/dev/null
 
 echo "Cloning rusty-boxen to /opt/rusty-boxen..."
-git clone --recurse-submodules https://github.com/michaeldfallen/rusty-boxen.git /opt/rusty-boxen
+sudo git clone --recurse-submodules https://github.com/michaeldfallen/rusty-boxen.git /opt/rusty-boxen
+sudo chown -R michael:michael /opt/rusty-boxen
 
 cd /opt/rusty-boxen
 
@@ -15,11 +18,14 @@ echo "Create ssh key..."
 ./bin/newsshkey "github.com"
 read -p "Upload ssh key to Github then press any key to continue... " -n1 -s </dev/tty
 
+git remote remove origin
+git remote add origin git@github.com:michaeldfallen/rusty-boxen
+
 echo "Boostrapping puppet..."
-apt-get install -y puppet >/dev/null
+sudo apt-get install -y puppet >/dev/null
 
 echo "Running hacks..."
-./hacks/facter_fqdn.sh
+sudo ./hacks/facter_fqdn.sh
 
 echo "Running rusty..."
 /opt/rusty-boxen/bin/rusty update
