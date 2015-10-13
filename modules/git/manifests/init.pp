ae886aa5fa1154df648d617b7eebc6adfdc0bed0
Michael Allen <michael@michaelallen.io>
2015-10-13 12:41:48 +0100

    Git config needs HOME set so it knows where to store config

--- modules/git/manifests/init.pp
+++ modules/git/manifests/init.pp
@@ -1,14 +1,19 @@
 #Install and configure git
-class git {
+class git (
+  $user = 'michael',
+  $git_username = 'Michael Allen',
+  $git_email = 'michael@michaelallen.io',
+) {
   package { 'git':
     ensure => present
   }
   $config = [
-    '/usr/bin/git config --global user.name "Michael Allen"',
-    '/usr/bin/git config --global user.email "michael@michaelallen.io"'
+    "/usr/bin/git config --global user.name '${git_username}'",
+    "/usr/bin/git config --global user.email '${git_email}'",
   ]
   exec { $config:
-    require => Package['git'],
-    user    => 'michael',
+    require     => Package['git'],
+    user        => $user,
+    environment => "HOME=/home/${user}"
   }
 }
