9bd1058a9f0aac430ce561213f6344343cc4bda3
Michael Allen <michael@michaelallen.io>
2015-10-13 15:41:13 +0100

    Set Meslo as preferred font

--- modules/fonts/manifests/init.pp
+++ modules/fonts/manifests/init.pp
@@ -1,10 +1,18 @@
 # Installs the Powerline fonts
 class fonts ( $user = 'michael' ) {
+  $preferred_font = 'Meslo LG S Regular for Powerline 14'
+  $current = 'gsettings get org.gnome.desktop.interface monospace-font-name'
+  $set_font = 'gsettings set org.gnome.desktop.interface monospace-font-name'
   $font_dir = '/opt/rusty-boxen/modules/fonts/files/fonts'
+
   exec { "${font_dir}/install.sh":
     cwd         => $font_dir,
     unless      =>
       '/usr/bin/test -f $HOME/.fonts/Meslo\ LG\ S\ Regular\ for\ Powerline.otf',
+    environment => "HOME=/home/${user}",
+    user        => $user,
+  } ~> exec { "${set_font} \"${preferred_font}\"":
+    unless      => "/usr/bin/test \"$(${current})\" -e \"${preferred_font}\"",
     environment => "HOME=/home/${user}",
     user        => $user,
   }
