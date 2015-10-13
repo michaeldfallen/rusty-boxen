7529aad5202adeb22d653a5cc5139632d1e62e4d
Michael Allen <michael@michaelallen.io>
2015-10-13 11:21:16 +0100

    Installing youcompleteme is a slow process

--- modules/vim/manifests/installation/youcompleteme.pp
+++ modules/vim/manifests/installation/youcompleteme.pp
@@ -6,8 +6,9 @@ class vim::installation::youcompleteme (
   require ::libs::python
 
   $ycm = "/home/${user}/.vim/bundle/YouCompleteMe"
-  exec { "${ycm}/install.py --clang-completer":
+  exec { "${ycm}/install.py":
     cwd         => $ycm,
+    timeout     => 1800,
     subscribe   => Vcsrepo[$ycm],
     refreshonly => true,
     require     => Vcsrepo[$ycm],
