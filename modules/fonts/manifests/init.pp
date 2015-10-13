f9f3d79abb295c07089ccb2585a9e742043623b4
Michael Allen <michael@michaelallen.io>
2015-10-13 15:24:07 +0100

    Install the powerline fonts

---
+++ modules/fonts/manifests/init.pp
@@ -0,0 +1,9 @@
+# Installs the Powerline fonts
+class fonts ( $user = 'michael' ) {
+  exec { './install.sh':
+    cwd         => '/opt/rusty-boxen/modules/fonts/files/fonts',
+    unless      =>
+      '/bin/test -f $HOME/.fonts/Meslo\ LG\ S\ Regular\ for\ Powerline.otf',
+    environment => "HOME=/home/${user}",
+  }
+}
