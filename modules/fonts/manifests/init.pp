fb2f7712642faca848db9080b12879073fb7da54
Michael Allen <michael@michaelallen.io>
2015-10-13 15:27:18 +0100

    Fully qualified path

--- modules/fonts/manifests/init.pp
+++ modules/fonts/manifests/init.pp
@@ -1,7 +1,8 @@
 # Installs the Powerline fonts
 class fonts ( $user = 'michael' ) {
-  exec { './install.sh':
-    cwd         => '/opt/rusty-boxen/modules/fonts/files/fonts',
+  $font_dir = '/opt/rusty-boxen/modules/fonts/files/fonts'
+  exec { "${font_dir}/install.sh":
+    cwd         => $font_dir,
     unless      =>
       '/bin/test -f $HOME/.fonts/Meslo\ LG\ S\ Regular\ for\ Powerline.otf',
     environment => "HOME=/home/${user}",
