81749ef28f7aa1b6bac033c91f33cd22aaa39346
Michael Allen <michael@michaelallen.io>
2015-10-13 15:43:34 +0100

    Fully qualify gsettings

--- modules/fonts/manifests/init.pp
+++ modules/fonts/manifests/init.pp
@@ -1,8 +1,9 @@
 # Installs the Powerline fonts
 class fonts ( $user = 'michael' ) {
   $preferred_font = 'Meslo LG S Regular for Powerline 14'
-  $current = 'gsettings get org.gnome.desktop.interface monospace-font-name'
-  $set_font = 'gsettings set org.gnome.desktop.interface monospace-font-name'
+  $mono_font = 'org.gnome.desktop.interface monospace-font-name'
+  $current = "/usr/bin/gsettings get ${mono_font}"
+  $set_font = "/usr/bin/gsettings set ${mono_font} \"${preferred_font}\""
   $font_dir = '/opt/rusty-boxen/modules/fonts/files/fonts'
 
   exec { "${font_dir}/install.sh":
@@ -11,7 +12,7 @@ class fonts ( $user = 'michael' ) {
       '/usr/bin/test -f $HOME/.fonts/Meslo\ LG\ S\ Regular\ for\ Powerline.otf',
     environment => "HOME=/home/${user}",
     user        => $user,
-  } ~> exec { "${set_font} \"${preferred_font}\"":
+  } ~> exec { $set_font:
     unless      => "/usr/bin/test \"$(${current})\" -e \"${preferred_font}\"",
     environment => "HOME=/home/${user}",
     user        => $user,
