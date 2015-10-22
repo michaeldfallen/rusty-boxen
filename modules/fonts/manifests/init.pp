4249fc482fd802bf96ca790a2e4eb7c167b778ae
Michael Allen <michael@michaelallen.io>
2015-10-22 15:55:29 +0100

    Use gsettings override for setting preferred font

--- modules/fonts/manifests/init.pp
+++ modules/fonts/manifests/init.pp
@@ -1,20 +1,15 @@
 # Installs the Powerline fonts
 class fonts ( $user = 'michael' ) {
-  $preferred_font = 'Meslo LG S for Powerline 12'
-  $mono_font = 'org.gnome.desktop.interface monospace-font-name'
-  $current = "/usr/bin/gsettings get ${mono_font}"
-  $set_font = "/usr/bin/gsettings set ${mono_font} \"${preferred_font}\""
-  $font_dir = '/opt/rusty-boxen/modules/fonts/files/fonts'
 
   exec { "${font_dir}/install.sh":
-    cwd         => $font_dir,
+    cwd         => '/opt/rusty-boxen/modules/fonts/files/fonts',
     unless      =>
       '/usr/bin/test -f $HOME/.fonts/Meslo\ LG\ S\ Regular\ for\ Powerline.otf',
     environment => "HOME=/home/${user}",
     user        => $user,
-  } ~> exec { $set_font:
-    unless      => "/usr/bin/test \"$(${current})\" -e \"${preferred_font}\"",
-    environment => "HOME=/home/${user}",
-    user        => $user,
+  } ~> gnome::gsettings { 'rusty-mono-font':
+    schema => 'org.gnome.desktop.interface',
+    key    => 'monospace-font-name',
+    value  => '"Meslo LG S for Powerline 12"',
   }
 }
