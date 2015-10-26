427af0366ee7d14963362358b650af6f9b4d80b1
Michael Allen <michael@michaelallen.io>
2015-10-26 13:47:48 +0000

    Ensure ruby build has executed before chmoding the version file

--- modules/libs/manifests/ruby.pp
+++ modules/libs/manifests/ruby.pp
@@ -3,9 +3,10 @@ class libs::ruby {
   package { 'ruby-dev':
     ensure => present
   }
-  exec { 'chmod g+w /usr/local/rbenv/version': }
   rbenv::plugin { 'sstephenson/ruby-build': }
-  rbenv::build { '2.2.2': global => true }
+  rbenv::build { '2.2.2':
+    global => true
+  } ~> exec { 'chmod g+w /usr/local/rbenv/version': }
 
   zsh::path { 'rbenv.zsh':
     content => 'export PATH="/usr/local/rbenv/bin:$PATH"',
