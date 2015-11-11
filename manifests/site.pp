5892eb1092b6c14467f52cd351d9aeb652f39d4b
Michael Allen <michael@michaelallen.io>
2015-11-11 09:32:00 +0000

    String concat works by joining an array, not + strings

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,9 +7,9 @@ Exec {
 }
 exec { 'apt-get update':
   command => '/usr/bin/apt-get update',
-  onlyif  => '/bin/sh -c "[ ! -f /var/cache/apt/pkgcache.bin ]' +
-    ' || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin' +
-    ' | /bin/grep . > /dev/null"',
+  onlyif  => join(['/bin/sh -c "[ ! -f /var/cache/apt/pkgcache.bin ]',
+    ' || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin',
+    ' | /bin/grep . > /dev/null"',]),
 }
 Package {
   require => Exec['apt-get update']
