a44950904155fac1dada29d9f760a87749a238ff
Michael Allen <michael@michaelallen.io>
2015-11-11 16:52:24 +0000

    Add missing lib for Spotify

--- modules/ubuntu/manifests/apps.pp
+++ modules/ubuntu/manifests/apps.pp
@@ -12,7 +12,13 @@ class ubuntu::apps {
     'caffeine',
     'gconf-editor',
     'dconf-editor',
+    'pulseaudio-equalizer',
   ]: }
+
+  package { 'libgcrypt11':
+    source   => '/opt/rusty-boxen/modules/ubuntu/files/libgcrypt11.deb',
+    provider => 'dpkg',
+  }
 
   ::ubuntu::autostart { 'caffeine-indicator': }
 }
