6760fae0bd210129ffe54a807b8042d3ae9f268a
Michael Allen <michael@michaelallen.io>
2015-11-14 22:27:40 +0000

    Prevent apt::source from adding release name to the source

--- modules/ubuntu/manifests/sources.pp
+++ modules/ubuntu/manifests/sources.pp
@@ -8,8 +8,8 @@ class ubuntu::sources {
 
   apt::source { 'arc-theme':
     location => 'http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.10/',
-    release  => '',
-    repos    => '/',
+    release  => '/',
+    repos    => ' ',
     key      => {
       'id'     => 'D0E482AB5DA85265F9DEBAE55A7D1D38BEB6D886',
       'source' => 'http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_15.10/Release.key'
