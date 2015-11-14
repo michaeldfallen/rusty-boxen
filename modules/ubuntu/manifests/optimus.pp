66c355368ced35928c83beb3363503643406150b
Michael Allen <michael@michaelallen.io>
2015-11-14 14:29:59 +0000

    Proper optimus support by using nvidia-prime over bumblebee-nvidia

--- modules/ubuntu/manifests/optimus.pp
+++ modules/ubuntu/manifests/optimus.pp
@@ -1,9 +1,16 @@
 class ubuntu::optimus {
+  package { [
+    'nvidia-352',
+    'nvidia-prime',
+  ]: } ~>
   package {[
     'bumblebee',
-    'bumblebee-nvidia',
     'primus',
     'linux-headers-generic',
     'mesa-utils',
+  ]: } ~>
+  exec {[
+    "/bin/sed -i 's/^Driver=$/Driver=nvidia/' /etc/bumblebee/bumblebee.conf",
+    "/bin/sed -i 's/nvidia-current/nvidia-352/' /etc/bumblebee/bumblebee.conf",
   ]: }
 }
