9472b0118f77987770888a52f78711078d2a1079
Michael Allen <michael@michaelallen.io>
2015-11-15 22:56:43 +0000

    Finally a way of actually setting gsettings

--- modules/ubuntu/manifests/config.pp
+++ modules/ubuntu/manifests/config.pp
@@ -1,7 +1,8 @@
-class ubuntu::config {
-  ::gnome::gsettings { 'sidebar-apps':
-    schema => 'com.canonical.Unity.Launcher',
-    key    => 'favorites',
-    value  => "['application://ubiquity.desktop', 'application://org.gnome.Nautilus.desktop', 'application://google-chrome.desktop', 'application://spotify.desktop', 'application://gvim.desktop', 'application://slack.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']",
+class ubuntu::config($user = 'michael') {
+  ::ubuntu::gsettings { 'org.gnome.desktop.interface gtk-theme':
+    value => 'Arc-Darker',
+  }
+  ::ubuntu::gsettings { 'com.canonical.Unity.Launcher favorites':
+    value  => "['application://ubiquity.desktop', 'application://org.gnome.Nautilus.desktop', 'application://google-chrome.desktop', 'application://spotify.desktop', 'application://gvim.desktop', 'application://slack.desktop', 'application://steam.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']",
   }
 }
---
+++ modules/ubuntu/manifests/gsettings.pp
@@ -0,0 +1,16 @@
+define ubuntu::gsettings (
+  $key = $name,
+  $value,
+  $user = 'michael',
+) {
+  $command = "gsettings set $key \"$value\""
+  exec {"set ${key} on user ${user}":
+    command     => "PID=\$(pgrep gnome-session) && export DBUS_SESSION_BUS_ADDRESS=\$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/\$PID/environ|cut -d= -f2-) && ${command}",
+    unless      => "gsettings get ${key} | grep -E \"^'{0,1}${value}'{0,1}\$\"",
+    logoutput   => 'on_failure',
+    provider    => shell,
+    user        => $user,
+    group       => $user,
+    environment => ['DISPLAY=:0'],
+  }
+}
