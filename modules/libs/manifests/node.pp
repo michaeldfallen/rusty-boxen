75cad1c4e931c8b89d2dbcd4e6c9de97b1aeccc5
Michael Allen <michael@michaelallen.io>
2016-02-03 19:46:22 +0000

    Install Node.JS

--- modules/libs/manifests/node.pp
+++ modules/libs/manifests/node.pp
@@ -1,9 +1,24 @@
 # Install nodejs and npm
-class libs::node {
+class libs::node ( $user='michael' ) {
   package {
     ['nodejs', 'npm']:
   } ~> file { '/usr/bin/node':
     ensure => 'link',
     target => '/usr/bin/nodejs',
+  }
+  file { "/home/${user}/.npm_packages":
+    ensure => directory,
+    owner  => $user,
+    group  => $user,
+  }
+  file { "/home/${user}/.npmrc":
+    ensure => link,
+    target => '/opt/rusty-boxen/modules/libs/files/dot_npmrc',
+  }
+  zsh::path { 'npm_binaries.zsh':
+    content => 'PATH="$HOME/.npm_packages/bin:$PATH"',
+  }
+  zsh::path { 'npm_mans.zsh':
+    content => 'MANPATH="$HOME/.npm_packages/share/man:$(manpath)"'
   }
 }
