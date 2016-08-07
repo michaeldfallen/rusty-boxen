322f3a3ac740931bafeb3cc2e894eede75526e96
Michael Allen <michael@michaelallen.io>
2016-08-07 17:51:39 +0100

    Add an alias for pruning merged branches

--- modules/git/manifests/init.pp
+++ modules/git/manifests/init.pp
@@ -44,5 +44,5 @@ class git (
     content => 'fpath=(~/.zsh $fpath)'
   }
 
-  zsh::alias { 'prune-branches': cmd => "git fetch --prune && git branch -r | awk \\\'{print \$1}\\\' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk \\\'{print \$1}\\\' | xargs git branch -d" }
+  zsh::alias { 'prune-branches': cmd => "git fetch --prune && git branch -r | awk \\\'{print \$1}\\\' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk \\\'{print \$1}\\\' | (xargs git branch -d || true)" }
 }
